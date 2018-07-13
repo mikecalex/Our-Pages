b7beb71d483346070998e49cb3fc22d315c3dff0
mikecalex <mikecalex@gmail.com>
2018-07-13 15:42:36 -0400

    Initial Commit and Setup

---
+++ app/javascript/react/containers/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+
+    return(
+      <h1>Boo yaa</h1>
+    )
+  }
+}
+
+export default App
