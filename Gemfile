e0c82b2511e51483e6366cc3c5a14b24104147cd
mikecalex <mikecalex@gmail.com>
2018-07-14 21:05:13 -0400

    Added Devise

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'webpacker', '~> 3.2'
+gem "devise"
 
 group :development do
   gem 'listen', '~> 3.0.5'
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,19 @@
 class StaticPagesController < ApplicationController
+  before_action :authorize_user, except: [:index, :show]
+
   def index
+    if current_user
+      render :'static_pages/index'
+    else
+      redirect_to '/users/sign_in'
+    end
+  end
+
+  protected
+
+  def authorize_user
+    if !user_signed_in? || !current_user.admin?
+      raise ActionController::RoutingError.new("Not Found")
+    end
   end
 end
